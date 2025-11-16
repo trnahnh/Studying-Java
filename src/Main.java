5c41dd36ca226dbc402fe4333c1167cb01d1cc3d
Anh D Tran <tran3ah@mail.uc.edu>
2025-11-15 23:23:46 -0500

    println

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("I like pizza!");
-        System.out.println("It's really good!");
+        System.out.print("I like pizza!\n");
+        System.out.print("It's really good!\n");
+        System.out.print("Buy me pizza!");
     }
 }
