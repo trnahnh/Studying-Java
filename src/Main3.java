c3416b7698bc8ea287708c9b04d5b531d6e7d044
Anh D Tran <tran3ah@mail.uc.edu>
2025-11-16 17:37:40 -0500

    User input handling

--- src/Main3.java
+++ src/Main3.java
@@ -8,7 +8,25 @@ public class Main3 {
         System.out.print("Enter your name: ");
         String name = scanner.nextLine();
 
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.print("What is your GPA? ");
+        double gpa = scanner.nextDouble();
+
+        System.out.print("Are you a student? (true/false): ");
+        boolean isStudent = scanner.nextBoolean();
+
         System.out.println("Hello " + name);
+        System.out.println("You are " + age + " years old");
+        System.out.println("Your GPA is " + gpa);
+
+        if (isStudent) {
+            System.out.println("You are a student!");
+        }
+        else {
+            System.out.println("You are NOT a student!");
+        }
 
         scanner.close();
     }
