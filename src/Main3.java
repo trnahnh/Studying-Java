61fc11396ee5e1417cbd5bd05d158c1ecc3bee91
Anh D Tran <tran3ah@mail.uc.edu>
2025-11-16 18:12:00 -0500

    Calculate area of a rectangle

--- src/Main3.java
+++ src/Main3.java
@@ -48,6 +48,20 @@ public class Main3 {
 
         // Calculate area of a rectangle
 
+        double width = 0;
+        double height = 0;
+        double area = 0;
+
+        System.out.print("Enter the width: ");
+        width = scanner.nextDouble();
+
+        System.out.print("Enter the height: ");
+        height = scanner.nextDouble();
+
+        area = width * height;
+
+        System.out.println("The area is: " + area + "cm²");
+
         scanner.close();
     }
 }
