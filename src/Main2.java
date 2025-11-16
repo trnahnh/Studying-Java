c28e0f6a94743a81f3184e3424e637eafa868f73
Anh D Tran <tran3ah@mail.uc.edu>
2025-11-16 17:16:49 -0500

    Variables

--- src/Main2.java
+++ src/Main2.java
@@ -3,7 +3,31 @@ public class Main2 {
     public static void main(String[] args) {
 
         int age = 19;
+        int year = 2025;
+        int quantity = 1;
 
-        System.out.println(age);
+        double price = 19.99;
+        double gpa = 3.5;
+        double temperature = -12.5;
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        boolean isStudent = true;
+        boolean forSale = false;
+        boolean isOnline = true;
+
+        String name = "Anh Tran";
+
+        /*
+            String food = "Pho Bo 5 Trung Chan";
+            String email = "tran3ah@mail.uc.edu";
+            String car = "Corvette";
+            String color = "black";
+         */
+
+        System.out.println("Hello " + name);
+        System.out.println("You are " + age + " years old");
     }
 }
