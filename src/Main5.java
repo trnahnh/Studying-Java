176a454433192cfcb18a33f07231930cb96f769c
Anh D Tran <tran3ah@mail.uc.edu>
2025-11-18 23:43:38 -0500

    Arithmetic Operators

---
+++ src/Main5.java
@@ -0,0 +1,17 @@
+public class Main5 {
+
+    // Arithmetic Operators
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 2;
+        int z;
+
+        // z = x + y
+        // z = x + y;
+        z = x * y;
+
+        System.out.println(z);
+    }
+}
