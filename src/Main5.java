c6821d10a4704c5acb5d90d99e82caf02d4a9594
Anh D Tran <tran3ah@mail.uc.edu>
2025-11-18 23:49:48 -0500

    Increment and Decrement Operators

--- src/Main5.java
+++ src/Main5.java
@@ -2,13 +2,13 @@ public class Main5 {
 
     // Arithmetic Operators
     // Augmented Assignment Operators
+    // Increment and Decrement Operators
 
     public static void main(String[] args) {
 
-        int x = 10;
-        int y = 2;
+        int x = 1;
 
-        x += y;
+        x += 1;
 
         System.out.println(x);
     }
