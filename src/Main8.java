430406bded07b1288f3179ae5824d8a42b5330af
Anh D Tran <tran3ah@mail.uc.edu>
2025-11-19 18:28:51 -0500

    Random Numbers

---
+++ src/Main8.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class Main8 {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int number;
+
+        number = random.nextInt(1, 6);
+
+        System.out.println(number);
+    }
+}
