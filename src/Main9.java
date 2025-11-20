a273e76a4667655e786972e3ef9674a2c2fa4644
Anh D Tran <tran3ah@mail.uc.edu>
2025-11-19 22:49:01 -0500

    Math Class

---
+++ src/Main9.java
@@ -0,0 +1,16 @@
+public class Main9 {
+    public static void main(String[] args) {
+
+        // System.out.println(Math.PI);
+        // System.out.println(Math.E);
+
+        double result;
+
+        result = Math.pow(2, 5);
+        result = Math.abs(-5);
+        result = Math.sqrt(9);
+        result = Math.round(3.14);
+
+        System.out.println(result);
+    }
+}
