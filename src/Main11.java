628191d01fb24b85569d3e62bbfb74c5959a3cf0
Anh D Tran <tran3ah@mail.uc.edu>
2025-11-29 17:24:05 -0500

    Compound Interest Calculation

---
+++ src/Main11.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main11 {
+    public static void main(String[] args) {
+
+        // Compound interest calculation
+
+        Scanner scanner = new Scanner(System.in);
+
+        double principal;
+        double rate;
+        int timesCompounded;
+        int years;
+        double amount;
+
+
+
+        scanner.close();
+    }
+}
