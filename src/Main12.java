e73bec3c9ee5d84c8545ad41c9780ba88dc39e51
Anh D Tran <tran3ah@mail.uc.edu>
2025-11-29 17:50:16 -0500

    nested if statements

---
+++ src/Main12.java
@@ -0,0 +1,33 @@
+public class Main12 {
+    public static void main(String[] args) {
+
+        // nested if statements
+
+        boolean isStudent = false;
+        boolean isSenior = false;
+        double price = 9.99;
+
+        if (isStudent) {
+            if (isSenior) {
+                System.out.println("You get a senior discount of 20%");
+                System.out.println("You get a student discount of 10%");
+                price *= 0.7;
+            }
+            else {
+                System.out.println("You get a student discount of 10%");
+                price *= 0.9;
+            }
+        }
+         else {
+             if (isSenior) {
+                 System.out.println("You get a senior discount of 20%");
+                 price *= 0.8;
+             }
+             else {
+                 price *= 1;
+             }
+        }
+
+        System.out.printf("The price of a ticket is: $%.2f", price);
+    }
+}
