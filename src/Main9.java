3594f2cbfc9c5601b20fce3ed420ce34d1424ed3
Anh D Tran <tran3ah@mail.uc.edu>
2025-11-29 16:41:37 -0500

    Continue

--- src/Main9.java
+++ src/Main9.java
@@ -1,16 +1,18 @@
+import java.util.Scanner;
+
 public class Main9 {
     public static void main(String[] args) {
 
-        // System.out.println(Math.PI);
-        // System.out.println(Math.E);
+        Scanner scanner = new Scanner(System.in);
 
-        double result;
+        double radius;
+        double circumference;
+        double area;
+        double volume;
 
-        result = Math.pow(2, 5);
-        result = Math.abs(-5);
-        result = Math.sqrt(9);
-        result = Math.round(3.14);
+        System.out.println("Enter the radius: ");
+        radius = scanner.nextDouble();
 
-        System.out.println(result);
+        scanner.close();
     }
 }
