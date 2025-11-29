a5476098e03c5c06af7d616f4c3816fdb09371e9
Anh D Tran <tran3ah@mail.uc.edu>
2025-11-29 16:54:46 -0500

    printf syntax

--- src/Main9.java
+++ src/Main9.java
@@ -10,8 +10,16 @@ public class Main9 {
         double area;
         double volume;
 
-        System.out.println("Enter the radius: ");
+        System.out.print("Enter the radius: ");
         radius = scanner.nextDouble();
+
+        circumference = 2 * Math.PI * radius;
+        area = Math.PI * Math.pow(radius, 2);
+        volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+
+        System.out.printf("The circumference is: %.1fcm\n", circumference);
+        System.out.printf("The circumference is: %.1fcm²\n", area);
+        System.out.printf("The volume is: %.1fcm³\n", volume);
 
         scanner.close();
     }
