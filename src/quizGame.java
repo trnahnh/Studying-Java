b65fa6d9480e0143708064c7dd73ca7d0376875e
Anh D Tran <tran3ah@mail.uc.edu>
2025-12-14 18:05:49 -0500

    Quiz Game Final

--- src/quizGame.java
+++ src/quizGame.java
@@ -29,7 +29,28 @@ public class quizGame {
 
         for (int i = 0; i < questions.length; i++) {
             System.out.println(questions[i]);
+
+            for (String option : options[i]) {
+                System.out.println(option);
+            }
+
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+
+            if (guess == answers[i]) {
+                System.out.println("********");
+                System.out.println("CORRECT!");
+                System.out.println("********");
+                score++;
+            }
+            else {
+                System.out.println("******");
+                System.out.println("WRONG!");
+                System.out.println("******");
+            }
         }
+
+        System.out.println("Your final score is: " + score + " out of " + questions.length);
 
         scanner.close();
     }
